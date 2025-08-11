367416929878bd114b6d414a0320c021838c09f3
modeco80 <lily.modeco80@protonmail.ch>
2025-08-10 21:39:40 -0400

    make headerizing global

--- make/rules.mk
+++ make/rules.mk
@@ -15,7 +15,15 @@ $(PNACH_FILENAME): $(PNACH_JSON) $(DEPS)
 # Use to generate an ASM rule
 # $1 = source file
 # $2 = A binary product of this ASM file
-define make_asm
+define make_asm_without_headerize
 $(2): $(1)
-	armips -sym $(dir $(2))/$(basename $(notdir $(1))).sym $(1)
+	armips -strequ region $(REGION) -sym $(dir $(2))$(basename $(notdir $(1))).sym $(1)
+endef
+
+define make_asm
+$(dir $(2))$(basename $(notdir $(1))).asm: $(1)
+	$(TOP)/make/headerize_asm.py $(1) $(dir $(2))$(basename $(notdir $(1))).asm $(REGION)
+
+$(2): $(dir $(2))$(basename $(notdir $(1))).asm
+	armips -strequ region $(REGION) -sym $(dir $(2))$(basename $(notdir $(1))).sym $(dir $(2))$(basename $(notdir $(1))).asm
 endef
